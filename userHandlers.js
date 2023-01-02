8e5ff8339f3e2234c9739524336d0862ea6e5f46
iturnmyscreenon <benoit.galey@gmail.com>
2023-01-02 09:44:20 +0100

    added route putUsers

--- userHandlers.js
+++ userHandlers.js
@@ -46,9 +46,30 @@ const postUsers = (req, res) => {
     });
 }
 
+const putUsers = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { firstname, lastname, email, city, language } = req.body;
+  
+  database
+
+    .query("update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?", [firstname, lastname, email, city, language, id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+}
+
 
 module.exports = {
   getUsers,
   getUserById,
-  postUsers
+  postUsers,
+  putUsers
 };
